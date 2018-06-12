cfc46538c3bdb425da0eec80725bcef80c839db1
yzdalhj <yzdalhj@163.com>
2018-06-12 11:16:28 +0800

    集成druid

---
+++ hbm/src/main/java/red/packet/hbm/HbmApplication.java
@@ -0,0 +1,12 @@
+package red.packet.hbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbmApplication.class, args);
+	}
+}
---
+++ hbm/src/test/java/red/packet/hbm/HbmApplicationTests.java
@@ -0,0 +1,16 @@
+package red.packet.hbm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
